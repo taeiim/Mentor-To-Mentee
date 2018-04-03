871006fdce08fab88fa6ccb43eac6a497293998b
서윤호 <seoyunho@192.168.nate.com>
2018-04-03 13:20:08 +0900

    [CREATE] user account route

--- server/app.js
+++ server/app.js
@@ -1,11 +1,10 @@
-"use strict";
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var app = express();
-
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
+const user = require('./routes/user/router')
 app.set('port', 8080);
 
 app.use(logger('dev'));
@@ -14,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/users', usersRouter);
+app.use('/', user);
 app.listen(app.get('port'), function () {
   console.log('Example app listening on' + app.get('port') + 'port');
 });
---
+++ server/routes/user/router.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const modules = require('./modules')
+const SHA256 = require('sha256')
+
+const router = express.Router()
+
+//signup
+router.route('/signup').post( (req, res) => {
+  const { id, pwd, name, age, phone} = req.body
+  
+  modules.signup(id, SHA256(pwd), name, age, phone, status => {
+    res.writeHead(status, { 'Content-Type': 'application/json'})
+    res.end()
+  })
+
+})
+
+//signin
+router.route('/signin').post( (req, res) => {
+  const { id, pwd } = req.body
+
+  modules.signin(id, SHA256(pwd), (status, jwt) => {  
+    res.writeHead(status, { 'Content-Type': 'application/json'})
+    if (status === 200) res.end(JSON.stringify({'Authorization' : jwt}))
+    res.end()
+  })
+})
+
+module.exports = router
